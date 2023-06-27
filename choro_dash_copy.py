dc0e0dffe32d021d210ac0d94246657ca402ec52
hannes <hannes.sikkink@gmail.com>
2023-06-27 11:17:05 -0400

    removed import os

--- choro_dash_copy.py
+++ choro_dash_copy.py
@@ -1,7 +1,6 @@
 import pandas as pd     
 import plotly           
 import plotly.express as px
-import os
 import dash             
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
